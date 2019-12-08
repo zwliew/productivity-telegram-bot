9991a3ca80a5c8a61b6991a6048ba7a0bdbbb341
Zhao Wei Liew <zhaoweiliew@gmail.com>
2019-12-08 15:19:03 +0800

    Bind to the port provided by Heroku

--- main.py
+++ main.py
@@ -203,6 +203,9 @@ if __name__ == '__main__':
     # the "static" directory. See:
     # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
     # App Engine itself will serve those files as configured in app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    port = os.envion.get('PORT')
+    if not port:
+        port = 8080
+    app.run(host='127.0.0.1', port=port, debug=True)
 
 main()
